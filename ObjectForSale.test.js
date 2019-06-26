22961e46e6bf05a1ed88f71d5216b046a2ed6052
Benjamin Caradeuc <benjamin@caradeuc.info>
2019-06-26 21:30:16 +0200

    ObjectForSale tests

--- ObjectForSale.test.js
+++ ObjectForSale.test.js
@@ -9,13 +9,38 @@ describe('ObjectForSale', () => {
   })
 
   describe('Successful auction', () => {
-    test.todo('1 buyer 1 bid above the reserve price')
-    test.todo('1 buyer multiple bids above the reserve price')
-    test.todo('multiple buyers 1 bid above the reserve price')
-    test.todo('multiple buyers multiple bids above the reserve price')
+    test('1 buyer & 1 bid above the reserve price', () => {
+      const testBuyer1 = new Buyer({ bids: [20]})
+      const testObject = new ObjectForSale({ reservePrice: 10, buyers: [testBuyer1] })
+      expect(testObject.getResult()).toEqual({ winner: testBuyer1, priceToPay: 10 })
+    })
+
+    test('1 buyer &multiple bids above the reserve price', () => {
+      const testBuyer1 = new Buyer({ bids: [20, 30]})
+      const testObject = new ObjectForSale({ reservePrice: 10, buyers: [testBuyer1] })
+      expect(testObject.getResult()).toEqual({ winner: testBuyer1, priceToPay: 10 })
+    })
+
+    test('multiple buyers & 1 bid above the reserve price', () => {
+      const testBuyer1 = new Buyer({ bids: [20]})
+      const testBuyer2 = new Buyer({ bids: [5]})
+      const testObject = new ObjectForSale({ reservePrice: 10, buyers: [testBuyer1, testBuyer2] })
+      expect(testObject.getResult()).toEqual({ winner: testBuyer1, priceToPay: 10 })
+    })
+
+    test('multiple buyers & multiple bids above the reserve price', () => {
+      const testBuyer1 = new Buyer({ bids: [20, 30]})
+      const testBuyer2 = new Buyer({ bids: [5, 15, 35]})
+      const testObject = new ObjectForSale({ reservePrice: 10, buyers: [testBuyer1, testBuyer2] })
+      expect(testObject.getResult()).toEqual({ winner: testBuyer2, priceToPay: 30 })
+    })
   })
 
   describe('No winner auction', () => {
-    test.todo('nobody bids above the reserve price')
+    test('nobody bids above the reserve price', () => {
+      const testBuyer1 = new Buyer({ bids: [2]})
+      const testObject = new ObjectForSale({ reservePrice: 10, buyers: [testBuyer1] })
+      expect(testObject.getResult()).toEqual(null)
+    })
   })
 })
